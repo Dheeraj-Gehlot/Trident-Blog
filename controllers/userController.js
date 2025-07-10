b53f6eddad85e471a87b0edc7b86d71f70ee34f6
Dheeraj <dheeraj1visionvivante@gmail.com>
2025-07-10 15:46:12 +0530

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,177 +1,453 @@
-import { genratePassword,createToken } from "../helper/helper.js";
+import { matchPassword, createToken } from "../helper/helper.js";
+import { CategoryMoldel } from "../models/categoryModel.js";
 import { PostDetails } from "../models/postModel.js";
-import {UserDetails} from '../models/User.js';
+import { UserDetails } from '../models/User.js';
 import schema from '../validation/schema.js'
-  
+import fs from 'fs';
+import path from 'path';
 
-// Render Home Page
-  const renderHomePage = async (req, res,next) => { 
+
+// Render Dashboard
+const renderDashboard = async (req, res, next) => {
     // let PostTabel = await PostDetails;
-    
-    await PostDetails.find().then((result,err)=>{
-        if(!err){
-            res.render('index',{
-                loginUrl: process.env.DOMAIN + '/login',
-                HomeUrl : process.env.DOMAIN + '/',
-                SignupUrl : process.env.DOMAIN + '/login',
-                posts: result
-             });
-        }
-    })
+    res.render('dashboard');
 };
 
 // Render Login Page
-  const renderLoginPage = (req, res) => {
+const renderLoginPage = (req, res) => {
 
-    res.render('login',{
+    res.render('login', {
         loginUrl: process.env.DOMAIN + '/login',
-        HomeUrl : process.env.DOMAIN + '/',  
-        SignupUrl : process.env.DOMAIN + '/signup',
-     });
+        HomeUrl: process.env.DOMAIN + '/',
+        SignupUrl: process.env.DOMAIN + '/signup',
+    });
 };
 
-// Render Signup Page
-  const renderSignupPage = (req, res) => {
-    res.render('signup',{
-        loginUrl: process.env.DOMAIN + '/login',
-        HomeUrl : process.env.DOMAIN + '/',  
-        SignupUrl : process.env.DOMAIN + '/signup',
-     });
-};  
 
-// Signup Handle
-const register = async(req,res,next)=>{ 
+const login = async (req, res, next) => {
     let request = req?.body;
     let errors = [];
-    const { username, email, password, confirm_password } = req.body;
-    
-    // Check required fields
-    if (!username || !email || !password || !confirm_password) {
-        errors.push({ msg: 'Please fill in all fields' });
-    }
+    const { email, password } = req.body;
+    console.log(req?.body, "chekc");
 
-    // Check passwords match
-    if (password !== confirm_password) {
-        errors.push({ msg: 'Passwords do not match' });
+    // Check required fields
+    if (!email || !password) {
+        errors.push({ msg: 'Please fill in all fields' });
     }
 
     // Check password length
     if (password.length < 6) {
         errors.push({ msg: 'Password should be at least 6 characters' });
     }
+
+
     if (errors.length > 0) {
-        res.render('signup', {
-            errors,
-            username,
-            email,
-            password,
-            confirm_password
-        });
-    }
-    try { 
-        let checkuser = await UserDetails.findOne({email:request?.email},{email:1}); 
-        if(checkuser?.email){
-            errors.push({ msg: 'Email is already registered' });
-            res.render('signup', {
-                errors,
-                username,
-                email,
-                password,
-                confirm_password
+        return res.status(400).json({
+            status: false,
+            errors: errors,
+        })
+    } else {
+        try {
+            let user = await UserDetails.findOne({ email: request?.email }, { email: 1, password: 1, password_salt: 1 });
+            if (!user) {
+                errors.push({ msg: 'Email is not registered' });
+                console.log(errors, "errors");
+                return res.status(400).json({
+                    status: false,
+                    errors: errors,
+                })
+            } else {
+
+                let pass = matchPassword(request?.password, user?.password, user?.password_salt);
+                console.log(pass, "pass");
+                if (!pass) {
+                    errors.push({ msg: 'Password is not correct' });
+                    return res.status(400).json({
+                        status: false,
+                        errors: errors,
+                    })
+                } else {
+                    let token = createToken(user?._id?.toString());
+                    return res.status(200).json({
+                        status: true,
+                        message: "User Logged In Successfully",
+                        token: token
+                    });
+                }
+            }
+        } catch (error) {
+            errors.push({ msg: error.message });
+            return res.status(400).json({
+                status: false,
+                errors: errors,
             });
         }
-        request.type = 'user';
-        let pass = genratePassword(request?.password)
-        _.extend(request,pass)
-        let user = await UserDetails.create(request);
-        let token = createToken(user?._id?.toString())
-        return res.status(200).json({
-            status:true,
-            message :"user Cretaed"+ user?._id?.toString(),
-            token :token
-        }) 
-    } catch (error) {  
-        errors.push({ msg: error.message  });
-            res.render('signup', {
-                errors,
-                username,
-                email,
-                password,
-                confirm_password
-            });
-            
     }
-}
+};
 
-  const signupUser = (req, res) => {
-    const { username, email, password, confirm_password } = req.body;
+const createblog = async (req, res, next) => {
+    let request = req?.body;
     let errors = [];
 
     // Check required fields
-    if (!username || !email || !password || !confirm_password) {
+    if (!request?.Title || !request?.Seo_Meta_Title || !request?.Heading || !request?.Description || !request?.Category) {
         errors.push({ msg: 'Please fill in all fields' });
     }
 
-    // Check passwords match
-    if (password !== confirm_password) {
-        errors.push({ msg: 'Passwords do not match' });
-    }
-
-    // Check password length
-    if (password.length < 6) {
-        errors.push({ msg: 'Password should be at least 6 characters' });
+    // Handle image upload
+    let imagePath = '';
+    if (req.file) {
+        // Folder name based on blog title (slugify for safety) 
+        const blogDir = path.join(process.cwd(), 'public', 'blog');
+        if (!fs.existsSync(blogDir)) {
+            fs.mkdirSync(blogDir, { recursive: true });
+        }
+        const ext = path.extname(req.file.originalname);
+        const fileName = Date.now() + ext;
+        const destPath = path.join(blogDir, fileName);
+        fs.writeFileSync(destPath, req.file.buffer);
+        imagePath = `/images/blog/${fileName}`;
     }
 
     if (errors.length > 0) {
-        res.render('signup', {
-            errors,
-            username,
-            email,
-            password,
-            confirm_password
+        return res.status(400).json({
+            status: false,
+            errors: errors,
         });
     } else {
-        // Validation passed
-        User.findOne({ email: email }).then(user => {
-            if (user) {
-                // User exists
-                errors.push({ msg: 'Email is already registered' });
-                res.render('signup', {
-                    errors,
-                    username,
-                    email,
-                    password,
-                    confirm_password
-                });
-            } else {
-                const newUser = new User({
-                    username,
-                    email,
-                    password
-                });
+        try {
+            let blog = new PostDetails({
+                Title: request?.Title,
+                Seo_Meta_Title: request?.Seo_Meta_Title,
+                Heading: request?.Heading,
+                Description: request?.Description,
+                Image: imagePath, // Save image path in DB
+                Category: request?.Category
+            });
+            await blog.save();
+            return res.status(200).json({
+                status: true,
+                message: "Blog created successfully"
+            });
+        } catch (error) {
+            errors.push({ msg: error.message });
+            return res.status(400).json({
+                status: false,
+                errors: errors,
+            });
+        }
+    }
+};
 
-                // Hash Password
-                bcrypt.genSalt(10, (err, salt) => bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err) throw err;
-                    // Set password to hashed
-                    newUser.password = hash;
-                    // Save user
-                    newUser.save()
-                        .then(user => {
-                            req.flash('success_msg', 'You are now registered and can log in');
-                            res.redirect('/login');
-                        })
-                        .catch(err => console.log(err));
-                }));
+const editblog = async (req, res, next) => {
+    let request = req?.body;
+    let errors = [];
+
+    // Check required fields
+    if (!request?.id || !request?.Title || !request?.Seo_Meta_Title || !request?.Heading || !request?.Description || !request?.Category) {
+        errors.push({ msg: 'Please fill in all fields' });
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).json({
+            status: false,
+            errors: errors,
+        });
+    } else {
+        try {
+            let blog = await PostDetails.findByIdAndUpdate(request?.id, {
+                Title: request?.Title,
+                Seo_Meta_Title: request?.Seo_Meta_Title,
+                Heading: request?.Heading,
+                Description: request?.Description,
+                // Image: imagePath, // Save image path in DB
+                Category: request?.Category
+            });
+            if (!blog) {
+                errors.push({ msg: 'Blog not found' });
+                return res.status(404).json({
+                    status: false,
+                    errors: errors,
+                });
             }
+            return res.status(200).json({
+                status: true,
+                message: "Blog updated successfully"
+            });
+        } catch (error) {
+            errors.push({ msg: error.message });
+            return res.status(400).json({
+                status: false,
+                errors: errors,
+            });
+        }
+    }
+};
+
+const viewblog = async (req, res, next) => {
+    let request = req?.query;
+    let errors = [];
+
+    // Check required fields
+    if (!request?.id) {
+        errors.push({ msg: 'Blog ID is required' });
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).json({
+            status: false,
+            errors: errors,
+        });
+    } else {
+        try {
+            let blog = await PostDetails.findById(request?.id);
+            if (!blog) {
+                errors.push({ msg: 'Blog not found' });
+                return res.status(404).json({
+                    status: false,
+                    errors: errors,
+                });
+            }
+            return res.status(200).json({
+                status: true,
+                blog: blog
+            });
+        } catch (error) {
+            errors.push({ msg: error.message });
+            return res.status(400).json({
+                status: false,
+                errors: errors,
+            });
+        }
+    }
+};
+
+const deleteblog = async (req, res, next) => {
+    let request = req?.query;
+    let errors = [];
+
+    // Check required fields
+    if (!request?.id) {
+        errors.push({ msg: 'Blog ID is required' });
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).json({
+            status: false,
+            errors: errors,
+        });
+    } else {
+        try {
+            let blog = await PostDetails.findByIdAndDelete(request?.id);
+            if (!blog) {
+                errors.push({ msg: 'Blog not found' });
+                return res.status(404).json({
+                    status: false,
+                    errors: errors,
+                });
+            }
+            return res.status(200).json({
+                status: true,
+                message: "Blog deleted successfully"
+            });
+        } catch (error) {
+            errors.push({ msg: error.message });
+            return res.status(400).json({
+                status: false,
+                errors: errors,
+            });
+        }
+    }
+};
+
+const blogList = async (req, res, next) => {
+    try {
+        let blogs = await PostDetails.find();
+        return res.status(200).json({
+            blogs: blogs
+        });
+    } catch (error) {
+        return res.status(400).json({
+            status: false,
+            errors: [{ msg: error.message }],
         });
     }
 };
 
+const createCategory = async (req, res, next) => {
+    let request = req?.body;
+    let errors = [];
+    // Check required fields
+    if (!request?.name) {
+        errors.push({ msg: 'Please fill in all fields' });
+    }
+    if (errors.length > 0) {
+        return res.status(400).json({
+            status: false,
+            errors: errors,
+        });
+    } else {
+        try {
+            let category = new CategoryMoldel({
+                name: request?.name,
+            });
+            await category.save();
+            return res.status(200).json({
+                status: true,
+                message: "Category created successfully"
+            });
+        } catch (error) {
+            errors.push({ msg: error.message });
+            return res.status(400).json({
+                status: false,
+                errors: errors,
+            });
+        }
+    }
+};
 
-export   {
-    renderHomePage,
+const getCategories = async (req, res, next) => {
+    try {
+        let categories = await CategoryMoldel.find();
+        return res.status(200).json({
+            status: true,
+            categories: categories
+        });
+    } catch (error) {
+        return res.status(400).json({
+            status: false,
+            errors: [{ msg: error.message }],
+        });
+    }
+};
+
+const deleteCategory = async (req, res, next) => {
+    let request = req?.query;
+    let errors = [];
+
+    // Check required fields
+    if (!request?.id) {
+        errors.push({ msg: 'Category ID is required' });
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).json({
+            status: false,
+            errors: errors,
+        });
+    } else {
+        try {
+            let category = await CategoryMoldel.findByIdAndDelete(request?.id);
+            if (!category) {
+                errors.push({ msg: 'Category not found' });
+                return res.status(404).json({
+                    status: false,
+                    errors: errors,
+                });
+            }
+            return res.status(200).json({
+                status: true,
+                message: "Category deleted successfully"
+            });
+        } catch (error) {
+            errors.push({ msg: error.message });
+            return res.status(400).json({
+                status: false,
+                errors: errors,
+            });
+        }
+    }
+};
+
+const updateCategory = async (req, res, next) => {
+    let request = req?.body;
+    let errors = [];
+    // Check required fields
+    if (!request?.id || !request?.name) {
+        errors.push({ msg: 'Please fill in all fields' });
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).json({
+            status: false,
+            errors: errors,
+        });
+    } else {
+        try {
+            let category = await CategoryMoldel.findByIdAndUpdate(request?.id, {
+                name: request?.name,
+            });
+            if (!category) {
+                errors.push({ msg: 'Category not found' });
+                return res.status(404).json({
+                    status: false,
+                    errors: errors,
+                });
+            }
+            return res.status(200).json({
+                status: true,
+                message: "Category updated successfully"
+            });
+        } catch (error) {
+            errors.push({ msg: error.message });
+            return res.status(400).json({
+                status: false,
+                errors: errors,
+            });
+        }
+    }
+};
+
+const viewcategory = async (req, res, next) => {
+    let request = req?.query;
+    let errors = [];
+
+    // Check required fields
+    if (!request?.id) {
+        errors.push({ msg: 'Category ID is required' });
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).json({
+            status: false,
+            errors: errors,
+        });
+    } else {
+        try {
+            let category = await CategoryMoldel.findById(request?.id);
+            if (!category) {
+                errors.push({ msg: 'Category not found' });
+                return res.status(404).json({
+                    status: false,
+                    errors: errors,
+                });
+            }
+            return res.status(200).json({
+                status: true,
+                category: category
+            });
+        } catch (error) {
+            errors.push({ msg: error.message });
+            return res.status(400).json({
+                status: false,
+                errors: errors,
+            });
+        }
+    }
+};
+
+export {
+    renderDashboard,
     renderLoginPage,
-    renderSignupPage,
-    register
-}
+    login,
+    createblog,
+    editblog,
+    viewblog,
+    deleteblog,
+    blogList,
+    createCategory,
+    getCategories,
+    deleteCategory,
+    updateCategory,
+    viewcategory
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,28 @@
 import express from 'express';
 const router = express.Router();
 
-import { renderHomePage, renderLoginPage, renderSignupPage,   register  } from '../controllers/userController.js';
-
+import { renderDashboard, renderLoginPage, login,createblog,editblog,viewblog,deleteblog,blogList, createCategory,updateCategory,deleteCategory,getCategories,viewcategory } from '../controllers/userController.js';
+import verifyToken from '../middleware/auth.js';
 // Home Page
-router.get('/', renderHomePage);
+// router.get('/', renderHomePage);
 
 // Login Page
-router.get('/login', renderLoginPage);
+router.get('/', renderLoginPage);
+router.get('/dashboard', renderDashboard);
 
-// Signup Page
-router.get('/signup', renderSignupPage);
+// Login Handle
+router.post('/login', login);
+router.post('/create-blog', verifyToken, createblog);
+router.post('/edit-blog', verifyToken, editblog);
+router.get('/view-blog', verifyToken, viewblog);
+router.delete('/delete-blog', verifyToken, deleteblog);
+router.get('/blog-list', verifyToken, blogList);
 
-// Signup Handle
-router.post('/signup', register);
-
+// category Page
+router.post('/create-category', verifyToken, createCategory);
+router.post('/edit-category', verifyToken, updateCategory);
+router.get('/view-category', verifyToken, viewcategory);
+router.delete('/delete-category', verifyToken, deleteCategory);
+router.get('/category-list', verifyToken, getCategories);
 
 export default router;
- 
